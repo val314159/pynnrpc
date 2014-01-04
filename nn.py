d4b7ba1e415403de1f9380c7e194ed939da17122
val314159 <jmward@gmail.com>
2014-01-04 16:14:16 -0600

    make a lot nicer

--- nn.py
+++ nn.py
@@ -5,6 +5,9 @@ from msgpack import loads, dumps
 #from coder import loads, dumps
 
 def send_recv(s,data):
+    """
+    convenience function to perform a send and a recv in one step.
+    """
     e = dumps(data)
     s.send(e)
     r = s.recv()
@@ -27,11 +30,11 @@ class RemoteBinder:
 
 def remote_binding_dict(binder):
     def fn2(s,d):
-        print "-"*80
-        print repr(d)
-        print "-"*80
+        #print "-"*80
+        #print repr(d)
+        #print "-"*80
         fn = binder.get(d[0], None)
-        print "FN", fn
+        #print "FN", fn
         xx = fn(*d[1],**d[2])
         return xx
     return fn2
@@ -41,3 +44,54 @@ def bind_remote_dict(s,binder):
 
 def serve_bindings(s,binder):
     while 1: bind_remote_dict(s,binder)
+
+_types = dict(PUB=PUB,SUB=SUB,REQ=REQ,REP=REP)
+
+def make_socket(url):
+    print "URL ", repr(url)
+    print url[0]
+    pfx = url[0]
+    url2 = url[1:]
+    transport, other = url2.split('::',1)
+    print "ARR1", transport, other
+    typ = _types[transport]
+    print "TYP ", typ
+    print "URL", other
+
+    print "QQQQ", other
+    arr = other.split('/',3)
+    print "ARR2", arr
+    subarr = arr[3:4]    
+    print "ARR3", subarr
+
+    newurl = '/'.join(arr[:3])
+    print "NU", newurl
+
+    s = Socket(typ)
+    if pfx == '+':
+        s.connect(newurl)
+    elif pfx == '-':
+        s.bind(newurl)
+
+    if typ == SUB  and  subarr:
+        print "DO IT"
+        subscribe(s, subarr[0])
+        pass
+
+    return s
+
+def subscribe(s, pat=''):
+    s.set_string_option(SUB, SUB_SUBSCRIBE, pat)
+    pass
+
+Socket.raw_recv = Socket.recv
+Socket.send_recv = Socket.send
+
+def recv2(s):
+    return loads(s.recv())
+
+def send2(s,d):
+    return dumps(s.send(d))
+
+Socket.recv2 = recv2
+Socket.send2 = send2
