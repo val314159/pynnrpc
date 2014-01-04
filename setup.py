8a591648edf2ddc0c4284e02b6d77b0616c269fe
val314159 <jmward@gmail.com>
2014-01-04 17:34:56 -0600

    add dep to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+requirements = [
+    'msgpack-python',
+]
 setup(name='pynnrpc',
       version='1.0',
       py_modules=['nn'],
+      install_requires=requirements,
       )
